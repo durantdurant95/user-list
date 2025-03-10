ecee931e6291216041269855b727a64fa04c4cbc
Alejandro Pérez Durán <apduran95@gmail.com>
2025-03-10 01:26:14 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>test</p>
+    </main>
+  );
+}
+
+export default App;
